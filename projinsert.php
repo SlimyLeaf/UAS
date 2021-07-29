08c85c55ba3ef8a5edf36e9e1515aa276b5e9970
SlimyLeaf <88050221+SlimyLeaf@users.noreply.github.com>
2021-07-29 22:46:19 +0700

    Update projinsert.php

--- projinsert.php
+++ projinsert.php
@@ -1,13 +1,8 @@
 <html>
-<body>
-
-Terima kasih telah melakukan pendaftaran sidang, silahkan tunggu info berikutnya untuk jadwal sidang!
-Data yang anda masukan adalah sebagai berikutnya
-
-NIM <?php echo $_POST["nim"]; ?><br>
-NAMA <?php echo $_POST["nama"]; ?><br>
-PROYEK <?php echo $_POST["proj"]; ?><br>
-JUDUL PROYEK <?php echo $_POST["judul proj"]; ?><br>
-
-</body>
-</html> 
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
