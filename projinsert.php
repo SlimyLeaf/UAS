f1c839c3ec4598cd2eb7d9b1e35b8065fd518c67
SlimyLeaf <88050221+SlimyLeaf@users.noreply.github.com>
2021-07-29 22:13:43 +0700

    Add files via upload

---
+++ projinsert.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Terima kasih telah melakukan pendaftaran sidang, silahkan tunggu info berikutnya untuk jadwal sidang!
+Data yang anda masukan adalah sebagai berikutnya
+
+NIM <?php echo $_POST["nim"]; ?><br>
+NAMA <?php echo $_POST["nama"]; ?><br>
+PROYEK <?php echo $_POST["proj"]; ?><br>
+JUDUL PROYEK <?php echo $_POST["judul proj"]; ?><br>
+
+</body>
+</html> 
